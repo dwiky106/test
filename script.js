138b38a5984bc087d450befbc2069a738b17ab30
dwiky106 <167574876+dwiky106@users.noreply.github.com>
2026-04-19 20:49:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// =======================
+// 🔥 IMPORT FIREBASE
+// =======================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+// =======================
+// 🔥 CONFIG FIREBASE
+// =======================
+const firebaseConfig = {
+  apiKey: "AIzaSyADQgZ1Pu0OmvWDJwDJgN6rPVt600ae1hg",
+  authDomain: "monitor-lele.firebaseapp.com",
+  projectId: "monitor-lele",
+  appId: "1:1060711241947:web:57517544c5ecbde01bd4fe"
+};
+
+// =======================
+// 🔥 INIT FIREBASE
+// =======================
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// =======================
+// 🔥 ELEMENT
+// =======================
+const form = document.getElementById("loginForm");
+const errorMsg = document.getElementById("errorMsg");
+
+// =======================
+// 🔥 LOGIN FUNCTION
+// =======================
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const userId = document.getElementById("userId").value;
+  const password = document.getElementById("password").value;
+
+  // 🔥 UBAH ID JADI EMAIL
+  const fakeEmail = userId + "@app.com";
+
+  // 🔥 LOGIN
+  signInWithEmailAndPassword(auth, fakeEmail, password)
+    .then(() => {
+      window.location.href = "dashboard/dashboard.html";
+    })
+    .catch(() => {
+      errorMsg.textContent = "ID atau password salah!";
+    });
+});
+
+// =======================
+// 🔥 TOGGLE PASSWORD
+// =======================
+const togglePassword = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+
+togglePassword.addEventListener("change", function () {
+  passwordInput.type = this.checked ? "text" : "password";
+});
